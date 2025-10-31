be09ec19f42d3fdbaffb4b4b59cfcf157ba32d56
Hasim <mhasim1441@gmail.com>
2025-10-31 16:26:49 +0530

    fix: Build error

--- src/layouts/dashboard/age-group-chart.tsx
+++ src/layouts/dashboard/age-group-chart.tsx
@@ -1,6 +1,4 @@
-"use client";
 
-import React from "react";
 import { LabelList, Pie, PieChart } from "recharts";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
